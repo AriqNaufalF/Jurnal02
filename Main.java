2ed19f40e67005f0972f92c6657ea185bc0a2931
AriqNaufalF <ariqn55@gmail.com>
2021-09-24 08:52:21 +0700

    menambahkan file Main.java

---
+++ Main.java
@@ -0,0 +1,11 @@
+/**
+ * Main
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Nama: Ariq Naufal Fadhlurrahman");
+        System.out.println("NIM: 6706213016");
+        System.out.println("Kelas: D3RPLA 45-05");
+    }
+}
