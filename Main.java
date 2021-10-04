4300319f3068a1186e62c276e2ebeb21d2fc9c63
AriqNaufalF <ariqn55@gmail.com>
2021-10-04 09:50:53 +0700

    menambahkan user input

--- Main.java
+++ Main.java
@@ -1,11 +1,28 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        //Variabel
         String nama, nim, kelas;
-        nama = "Ariq Naufal Fadhlurrahman";
+        float ipk;
+        /*nama = "Ariq Naufal Fadhlurrahman";
         nim = "6706213016";
         kelas = "D3RPLA 45-05";
-        float ipk = 4;
+        float ipk = 4;*/
+        System.out.print("Nama: ");
+        nama = input.nextLine();
+
+        System.out.print("NIM: ");
+        nim = input.nextLine();
+
+        System.out.print("Kelas: ");
+        kelas = input.nextLine();
+
+        System.out.print("IPK: ");
+        ipk = input.nextFloat();
 
 
         System.out.println("Nama: "+ nama);
