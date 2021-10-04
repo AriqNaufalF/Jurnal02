ab91a830bcfd31ff590367bcb967965ac91242d4
AriqNaufalF <ariqn55@gmail.com>
2021-10-04 09:22:42 +0700

    menambahkan variabel

--- Main.java
+++ Main.java
@@ -1,8 +1,16 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Nama: Ariq Naufal Fadhlurrahman");
-        System.out.println("NIM: 6706213016");
-        System.out.println("Kelas: D3RPLA 45-05");
+        String nama, nim, kelas;
+        nama = "Ariq Naufal Fadhlurrahman";
+        nim = "6706213016";
+        kelas = "D3RPLA 45-05";
+        float ipk = 4;
+
+
+        System.out.println("Nama: "+ nama);
+        System.out.println("NIM: " + nim);
+        System.out.println("Kelas: " + kelas);
+        System.out.println("IPK: " + ipk);
     }
 }
